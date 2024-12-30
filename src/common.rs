02c88bfa4b33030d23377bcd8c6ed267f477c02b
zengjie2005 <43885175+zengjie2005@users.noreply.github.com>
2024-12-30 19:17:47 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -909,7 +909,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://10.10.10.3:21120".to_owned()
+    "http://zengjienas.oicp.net:21120".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
